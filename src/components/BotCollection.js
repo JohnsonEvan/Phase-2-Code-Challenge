e35cb9d379c98f4a1aef691f78c38c23bd878882
Evan <evan.johnson99@yahoo.com>
2022-03-04 10:37:20 -0500

    attempted challege

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-function BotCollection() {
+
+// create a new variable with the components of the bots arrary and call the BotCard
+function BotCollection(bots) {
+  const BotsList = bots.map(bots => {
+    return ( <botcard key={bots.id}
+      )
+  }
+  
   // Your code here
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
+        {BotsList}
       </div>
     </div>
   );
--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
-  //start here with your code for step one
 
+// declare both arrays we will be using using state
+  const [bots, setBots] = useState ([]);
+  const [botArmy, setbotArmy] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+    .then(response => response.json())
+    .then(botsArray => setBots(botsArray))
+  },[]);
+
+  const handleDelete = (e) => {
+    const name = e.target.getAttribute("name")
+     updateList(list.filter(item => item.name !== name));
+   };
+//function handleOnClick
+
+
+//pass in both state arrays into the components below to give vaiable access to the Bots and bots army array 
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection />
+      <YourBotArmy botArmy={botArmy} handleDelete={handleDelete} handleOnClick={handleOnClick}/>
+      <BotCollection bots={bots} handleDelete={handleDelete} handleOnClick={handleOnClick}/>
     </div>
   )
 }
